c5f6928ae3e088c48e3c030f1725de461452d169
Dimitri Fontenelle <dimitriftl@hotmail.com>
2023-06-20 17:23:54 +0200

    commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,30 +1,11 @@
-import { useState, useEffect } from 'react'
 import './Home.css'
-import PokemonCard from '../../components/PokemonCard/PokemonCard'
+import PokemonList from '../../components/PokemonList/PokemonList'
 
 function Home() {
-  const pokemonsUrl = Array.from({ length: 151 }, (v, k) => 'https://pokeapi.co/api/v2/pokemon/' + (k + 1))
-  const [pokemons, setPokemons] = useState([])
-
-  useEffect(() => {
-    const pokemonsPromise = pokemonsUrl.map((url) => fetchData(url))
-
-    Promise.all(pokemonsPromise).then((values) => {
-      setPokemons(values)
-    })
-  }, [])
-
-  const fetchData = async (url) => await fetch(url).then(response => response.json())
-
   return (
     <div className="homeContainer">
       <div className="container">
-        {pokemons &&
-          pokemons.map(pokemon => (
-            <PokemonCard key={pokemon.name} pokemonImg={pokemon.sprites.other.dream_world.front_default} pokemonName={pokemon.name} pokemonTypes={pokemon.types} />
-
-          ))
-        }
+        <PokemonList />
       </div>
     </div>
   )
