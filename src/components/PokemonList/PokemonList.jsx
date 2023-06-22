27042bec3952a5158fca645b7ba1aff059446e36
Dimitri Fontenelle <dimitriftl@hotmail.com>
2023-06-22 15:33:16 +0200

    achievement

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -1,15 +1,11 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import ReactPaginate from 'react-paginate'
 import './PokemonList.css'
 import PokemonCard from '../PokemonCard/PokemonCard'
-import { useDispatch, useSelector } from 'react-redux'
-import { initPokemons } from '../../features/pokemon/pokemonSlice'
+import { useSelector } from 'react-redux'
 
 function Paginate() {
-  const dispatch = useDispatch()
   const pokemons = useSelector((state) => state.pokemons.data)
-
-  const pokemonsUrl = Array.from({ length: 151 }, (v, k) => 'https://pokeapi.co/api/v2/pokemon/' + (k + 1))
   const [currentPage, setCurrentPage] = useState(0)
   const PER_PAGE = 18
   const offset = currentPage * PER_PAGE;
@@ -17,15 +13,6 @@ function Paginate() {
     .slice(offset, offset + PER_PAGE)
     .map((pokemon) => <PokemonCard key={pokemon.name} pokemonImg={pokemon.sprites.other.dream_world.front_default} pokemonName={pokemon.name} pokemonTypes={pokemon.types} pokemonId={pokemon.id} />)
   const pageCount = Math.ceil(pokemons.length / PER_PAGE)
-
-  // Get PokemonList
-  const fetchData = async (url) => await fetch(url).then(response => response.json())
-  useEffect(() => {
-    const pokemonsPromise = pokemonsUrl.map((url) => fetchData(url))
-    Promise.all(pokemonsPromise).then((values) => {
-      dispatch(initPokemons(values))
-    })
-  }, [])
 
   // Change page
   function handlePageClick({ selected: selectedPage }) {
